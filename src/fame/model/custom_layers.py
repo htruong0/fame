94d24695714cb184ae5929c6e0e9fbeb34165fca
htruong0 <htruong@ip3-login.phyip3.dur.ac.uk>
2022-02-23 12:48:25 +0000

    add custom layers for frugally-deep

---
+++ src/fame/model/custom_layers.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+@tf.keras.utils.register_keras_serializable()
+class CoefSinhLayer(layers.Layer):
+    def __init__(self, coef_scale, **kwargs):
+        super(CoefSinhLayer, self).__init__(**kwargs)
+        self.coef_scale = coef_scale
+        
+    def call(self, inputs):
+        return tf.multiply(self.coef_scale, tf.math.sinh(inputs))
+    
+    def get_config(self):
+        config = super().get_config()
+        config["coef_scale"] = self.coef_scale
+        return config
+
+@tf.keras.utils.register_keras_serializable()
+class LogLayer(layers.Layer):
+    def __init__(self, **kwargs):
+        super(LogLayer, self).__init__(**kwargs)
+        
+    def call(self, inputs):
+        return tf.math.log(inputs)
+    
+    def get_config(self):
+        config = super().get_config()
+        return config
