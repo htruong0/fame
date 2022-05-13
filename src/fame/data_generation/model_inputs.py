6a83ef758403973041f405664a2304c4757fb8c1
htruong0 <htruong@ip3-login.phyip3.dur.ac.uk>
2022-05-13 15:40:03 +0100

    update model input calculator

--- src/fame/data_generation/model_inputs.py
+++ src/fame/data_generation/model_inputs.py
@@ -2,8 +2,10 @@ import numpy as np
 import tensorflow as tf
 from itertools import combinations
 
-from fame.utilities import utility_functions
-from fame.data_generation import relevantDipoles
+from fame_pp.utilities import utility_functions, pspoint
+from fame_pp.data_generation import relevantDipoles, cs_dipole
+
+CS = cs_dipole.CS_dipole()
 
 def get_relevant_permutations(num_jets):
     perms = relevantDipoles.getPermutations(num_jets)
@@ -16,11 +18,12 @@ def calc_phi(p, i, j):
     to our defined north on the plane perpendicular to the 
     averaged direction of the chosen indices i and j.
     '''
-    pijv = p[:, i+1, 1:] + p[:, j+1, 1:]
+    pijv = p[:, i-1, 1:] + p[:, j-1, 1:]
     p2 = np.sum(pijv*pijv, axis=1)
-    l = p2 / pijv[:, 2]
+    # take reference to be x direction because we can have initial state radiation now
+    l = p2 / pijv[:, 0]
     ez = np.zeros((len(p), 3))
-    ez[:, 2] = l
+    ez[:, 0] = l
     nord = ez - pijv
     nord = nord / np.linalg.norm(nord, axis=1)[:, None]
     east = np.cross(nord, pijv)
@@ -28,7 +31,7 @@ def calc_phi(p, i, j):
     assert np.allclose(np.sum(nord*pijv, axis=1), 0)
     assert np.allclose(np.sum(east*pijv, axis=1), 0)
 
-    piv = p[:, i+1, 1:]
+    piv = p[:, i-1, 1:]
     ei = piv - (np.sum(pijv*piv, axis=1) / p2)[:, None]*pijv
     ei = ei / np.linalg.norm(ei, axis=1)[:, None]
     assert np.allclose(np.sum(ei*pijv, axis=1), 0)
@@ -38,20 +41,13 @@ def calc_phi(p, i, j):
     phi = np.arctan2(sin, cos)
     return phi
 
-def calculate_cs_phis(p, num_jets, mode='gluon', cast=False):
+def calculate_cs_phis(p, cast=False):
     '''Given momenta calculate cos(2phi) and sin(2phi) for all gluon pairs with relevant prefactors.'''
     alpha_s = 0.118
-    if mode == 'gluon':
-        C_A = 3
-        prefactor = 16*np.pi*alpha_s*C_A
-        combs = list(combinations(range(3, num_jets+1), 2))
-    elif mode == 'quark':
-        T_R = 0.5
-        prefactor = 8*np.pi*alpha_s*T_R
-        # combinations for non-identical flavoured quarks in final state
-#         combs = [(1, 2), (1, 4), (2, 3), (3, 4)]
-        # combinations for identical flavoured quarks in final state
-        combs = [(1, 2), (3, 4)]
+    C_A = 3
+    prefactor = 16*np.pi*alpha_s*C_A
+    # skip 2 initial state gluon splitting
+    combs = list(combinations(CS.gluons, 2))[1:]
     
     cos_2phis = []
     sin_2phis = []
@@ -59,7 +55,7 @@ def calculate_cs_phis(p, num_jets, mode='gluon', cast=False):
         i = comb[0]
         j = comb[1]
         phis = calc_phi(p, i, j)
-        sij = 2*utility_functions.dot(p[:, i+1], p[:, j+1])
+        sij = 2*utility_functions.dot(p[:, i-1], p[:, j-1])
     
         cos_2phi = prefactor * np.cos(2*phis) / sij
         sin_2phi = prefactor * np.sin(2*phis) / sij
@@ -90,17 +86,26 @@ class ModelInputsGenerator():
         self.CS.set_momenta(p)
         D = []
         for perm in self.permutations:
-            D.append(self.CS.calculate_D_ijk(*perm))
+            D.append(self.CS.calculate_D(*perm))
         return tf.convert_to_tensor(D)
     
-    def calculate_ys(self, p):
+    def calculate_recoil_factors(self, p):
         '''Calculate Catani-Seymour recoil factors.'''
         self.CS.set_momenta(p)
-        y = []
+        r = []
         for perm in self.permutations:
             self.CS.set_indices(*perm)
-            y.append(self.CS.calculate_y())
-        return tf.convert_to_tensor(y)
+            self.CS.calculate_invariants()
+            rf = self.CS.calculate_recoil_factor()
+            # this step makes the x recoil input more like the FF y_ijk
+            if self.CS.mode in ["FI", "IF", "II"]:
+                rf = rf / (1-rf)
+            r.append(rf)
+        return tf.convert_to_tensor(r)
+
+    def calculate_mandelstam_invariants(self, p):
+        s = list(pspoint.PSpoint(p).sij.values())
+        return tf.convert_to_tensor(s)
     
     @tf.function
     def calculate_inputs(self, p_array, to_concat=None):
@@ -108,5 +113,6 @@ class ModelInputsGenerator():
         dipoles = tf.vectorized_map(self.calculate_Ds, p_array)
         if to_concat:
             dipoles = tf.concat([dipoles, *to_concat], axis=1)
-        ys = tf.vectorized_map(self.calculate_ys, p_array)
-        return dipoles, ys
+        rfs = tf.vectorized_map(self.calculate_recoil_factors, p_array)
+        sijs = tf.vectorized_map(self.calculate_mandelstam_invariants, p_array)
+        return dipoles, rfs, sijs
